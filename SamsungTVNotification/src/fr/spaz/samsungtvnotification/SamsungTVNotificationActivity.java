fd3c763b08d37a61bcbc845ab8665c11ee2bfe43
jdenadai <julien.denadai@gmail.com>
2012-04-17 21:13:43 +0200

    Initial import

---
+++ SamsungTVNotification/src/fr/spaz/samsungtvnotification/SamsungTVNotificationActivity.java
@@ -0,0 +1,13 @@
+package fr.spaz.samsungtvnotification;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SamsungTVNotificationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
